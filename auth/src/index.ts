29ecf91631821a9a3eb712fa85d5c349eec05fe5
Indrajith V <indrajithvgp@yahoo.com>
2021-05-19 17:09:39 +0530

    added ingress yaml file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,4 +5,8 @@ import {json} from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get("/api/users/currentuser", (req, res) =>{
+    res.send('<h1>GET it is !!!</h1>')
+})
+
 app.listen(3000, ()=> console.log(chalk.green('Running on port 3000')))
