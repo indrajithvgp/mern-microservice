3730ac8e79ec7bbf5d8b4f0117732503ea508aa5
Indrajith V <indrajithvgp@yahoo.com>
2021-07-29 21:10:57 +0530

    next client added

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -3,6 +3,10 @@ import mongoose from "mongoose"
 import {app} from '../app'
 
 
+// declare global {
+//     var signin: () => Promise<string[]>;
+//   }
+
 let mongo: any
 beforeAll(async () => {
     
