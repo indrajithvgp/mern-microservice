24abfae8279fdcdb2687fed036cdb53ee52df0c0
Indrajith V <indrajithvgp@yahoo.com>
2021-08-13 16:52:17 +0530

    client listener added for ticket events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import {natsWrapper} from '../src/nats-wrapper'
 import { app } from './app';
 
 const start = async () => {
@@ -11,6 +11,15 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'random', 'http://nats-srv:4222')
+    
+    natsWrapper.client.on('close', ()=>{
+            console.log('NATS Connection failed')
+            process.exit()
+        })
+        process.on('SIGINT', ()=> natsWrapper.client.close())
+        process.on('SIGTERM', ()=> natsWrapper.client.close())
+        
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+
+import {TicketUpdatedPublisher} from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper} from '../nats-wrapper'
+
+
 import {
   validateRequest,
   NotFoundError,
@@ -36,6 +41,12 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    })
 
     res.send(ticket);
   }
