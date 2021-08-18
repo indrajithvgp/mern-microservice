7c5e03baa30d000fb3cf501d8f2823d3d13aca4f
Indrajith V <indrajithvgp@yahoo.com>
2021-08-18 18:02:49 +0530

    ticket status fn implemented

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest, NotFoundError } from '@cygnetops/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@cygnetops/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15*60
 
 router.post(
   '/api/orders',
@@ -27,16 +29,35 @@ router.post(
       throw new NotFoundError();
     }
 
+
     // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved()
+    if(isReserved){
+      throw new BadRequestError("Ticket is already reserved");
+    }
 
     // Calculate an expiration date for this order
-
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds()+ EXPIRATION_WINDOW_SECONDS);
     // Build the order and save it to the database
-
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    })
+    await order.save();
     // Publish an event saying that an order was created
 
-    res.send({});
+    res.status(201).send(order);
   }
 );
+
+
+// declare global {
+//   var signin: () => string[];
+// }
+
+
 
 export { router as newOrderRouter };
